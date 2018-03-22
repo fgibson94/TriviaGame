3eb7c45064752cecdb3d1c591241a05e73dee58a
fgibson94 <gibson.faithlee@yahoo.com>
2018-03-22 15:32:01 -0400

    3222018

---
+++ assets/javascript/script.js
@@ -0,0 +1,5 @@
+    console.log("hello")
+    //timer set for 30sec
+    //question 1,2,3 with correct answer marked
+    //timeout goes to submit/click on submit
+    //page showing results of quiz
